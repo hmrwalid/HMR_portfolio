a1b462dda777b3434846fa450d63fa53cf679a3a
hamrouni walid <hmrwalid97@gmail.com>
2023-08-20 14:03:14 +0000

    navbar-portfolio

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+   <h1 className="text-4xl font-bold font-signature ">
+   <Navbar/>
+   </h1>
+  );
+}
+
+export default App;
